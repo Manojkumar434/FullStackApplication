6cf8b56f6c8d4eccd5dc6127ee6c3b3980f0c51f
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-03-03 13:53:53 +0530

    basic program

--- front-end/src/added.js
+++ front-end/src/added.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Firsttwo()
 {
-    return(<h1>That project name is Indian spices details,this project was developed by some masala agencies</h1>)
+    return(<h6>This project name is Indian spices details,this project was developed by some masala agencies</h6>)
 }
 
 export {Firsttwo}
