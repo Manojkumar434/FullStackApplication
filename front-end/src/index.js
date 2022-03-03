6b3b45aed4ee8d2e9cc67f3e9c7e61e2817046bb
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-03-03 11:36:53 +0530

    project created and executed is successfully added

---
+++ front-end/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//render-("what to show","where to show")
+ReactDOM.render(
+  <h1>Basic react js application techincal </h1>
+  ,document.getElementById('root')
+)
